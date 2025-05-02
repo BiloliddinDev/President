7e8c4f883d238f51045989c6705d666df06edb64
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2025-05-02 10:54:37 +0500

    add: product videos section

--- app/[lang]/(home)/page.tsx
+++ app/[lang]/(home)/page.tsx
@@ -1,31 +1,34 @@
-import {Showcase} from "@/app/[lang]/(home)/components/showcase";
-import {News} from "@/app/[lang]/(home)/components/news";
-import {About} from "./components/about";
-import {Category} from "@/app/[lang]/(home)/components/category";
-import {Collections} from "@/app/[lang]/(home)/components/Collections";
+import { Showcase } from "@/app/[lang]/(home)/components/showcase";
+import { News } from "@/app/[lang]/(home)/components/news";
+import { About } from "./components/about";
+import { Category } from "@/app/[lang]/(home)/components/category";
+import { Collections } from "@/app/[lang]/(home)/components/Collections";
+import { ProductVideos } from "./components/productVideos";
 
 export default function Home() {
-    return (
-        <>
-            <section>
-                <Showcase/>
-            </section>
-            <section className="mt-[100px]">
-                <News title={"News"}/>
-            </section>
-            <section className="mt-[100px]">
-                <About title={"About"}/>
-            </section>
-            <section className={"mt-[100px]"}>
-                <Category/>
-            </section>
-            <section className={"mt-[100px]"}>
-                <Collections/>
-            </section>
-            <section className="mt-[100px]">
-                <News title={"President stories"}/>
-            </section>
-        </>
-
-    );
+  return (
+    <>
+      <section>
+        <Showcase />
+      </section>
+      <section className="mt-[100px]">
+        <News title={"News"} />
+      </section>
+      <section className="mt-[100px]">
+        <About title={"About"} />
+      </section>
+      <section className={"mt-[100px]"}>
+        <Category />
+      </section>
+      <section className={"mt-[100px]"}>
+        <Collections />
+      </section>
+      <section className="mt-[100px]">
+        <ProductVideos title={"Product Videos"} />
+      </section>
+      <section className="mt-[100px]">
+        <News title={"President stories"} />
+      </section>
+    </>
+  );
 }
