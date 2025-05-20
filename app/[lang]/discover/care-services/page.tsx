6a17eec137ca87ff59592a34caf582f9a7640a6f
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2025-05-20 14:16:19 +0500

    feat: add 4-page to discover

--- app/[lang]/discover/care-services/page.tsx
+++ app/[lang]/discover/care-services/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React from "react";
 import Pen from "@/public/images/Pen.png";
 import { BreadcrumbDynamic } from "@/components/shared/breadcrumb-dynamic/breadcrumb-dynamic";
+import { careServicesItems } from "@/constants/care-services-items";
+import IconComponent from "@/components/shared/icon-view";
 const CareAndServices = () => {
   return (
     <div className="container max-w-screen-xl mx-auto px-2 md:px-4 mt-16 md:!mt-20">
@@ -10,6 +12,24 @@ const CareAndServices = () => {
       </div>
       <div className="-mx-[calc((100vw-100%)/2)] w-screen ">
         <Image src={Pen} alt="a pen" />
+      </div>
+      <div className="mt-[100px] mb-[200px]">
+        <h5 className="text-gray-900 text-lg font-medium leading-7 mb-7">
+          Care & Services
+        </h5>
+        <div className="flex gap-5 justify-between flex-wrap md:flex-nowrap">
+          {careServicesItems.map((item) => (
+            <div
+              key={item.id}
+              className="w-full px-9 py-7 flex items-center justify-between rounded gap-6 outline-1 outline-offset-[-1px] outline-gray-200"
+            >
+              <p className="text-sm font-medium leading-tight">
+                {item.serviceType}
+              </p>
+              <IconComponent name={item.iconName} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ constants/care-services-items.tsx
@@ -0,0 +1,22 @@
+export const careServicesItems = [
+  {
+    id: 1,
+    serviceType: "Writng Instrument",
+    iconName: "pen",
+  },
+  {
+    id: 2,
+    serviceType: "Watches",
+    iconName: "watch",
+  },
+  {
+    id: 3,
+    serviceType: "Leather",
+    iconName: "leather",
+  },
+  {
+    id: 4,
+    serviceType: "Accessories",
+    iconName: "accessories",
+  },
+];
