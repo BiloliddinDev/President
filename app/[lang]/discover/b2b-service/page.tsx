f95c12c9b265b1ea995228355a7d05e2578e26f3
Biloliddin_dev <bilol9876@gmail.com>
2025-05-28 20:59:44 +0500

    add b2b UI discover-modal item add

---
+++ app/[lang]/discover/b2b-service/page.tsx
@@ -0,0 +1,25 @@
+import {BreadcrumbDynamic} from "@/components/shared/breadcrumb-dynamic/breadcrumb-dynamic";
+import Link from "next/link";
+import {B2bCard} from "@/app/[lang]/discover/components/b2b-card/b2b-card";
+
+export default function B2BPage() {
+    return (
+        <div className={"container !mt-21"}>
+            <BreadcrumbDynamic/>
+            <div className={"flex justify-between items-center mt-16 "}>
+                <h2 className={"text-lg whitespace-pre-wrap"}>President Business Gifts B2B</h2>
+                <div className={"w-1/2"}>
+                    <p className={"text-sm mb-5"}>Corporate gifts are the perfect opportunity to show appreciation,
+                        reward
+                        trust or foster
+                        encouragement for any business occasion.</p>
+                    <Link className={"underline "} href={"#"}>Contact us for enquiries</Link>
+                </div>
+            </div>
+            
+            <div>
+                <B2bCard className={"mt-[100px]"}/>
+            </div>
+        </div>
+    )
+}
--- constants/discover-modal-item.tsx
+++ constants/discover-modal-item.tsx
@@ -1,6 +1,6 @@
 export const discoverModalItems = [
   { id: 1, linkSrc: "/discover/about-us", name: "About us" },
-  { id: 2, linkSrc: "/", name: "President B2B" },
+  { id: 2, linkSrc: "/discover/b2b-service", name: "President B2B" },
   { id: 3, linkSrc: "/discover/fine-watchmaking", name: "Fine Watchmaking" },
   { id: 4, linkSrc: "/discover/care-services", name: "Care & Services" },
   { id: 5, linkSrc: "/discover/corporate-gifts", name: "Corporate gifts" },
