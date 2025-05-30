7f4864b5964bc4f70523e70090c0514c0d35c877
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2025-05-30 10:52:41 +0500

    add: up to date with main

--- app/[lang]/(home)/components/showcase.tsx
+++ app/[lang]/(home)/components/showcase.tsx
@@ -1,39 +1,35 @@
-import {FC} from 'react';
-import {Button} from "@/components/ui/button";
-import {imageLoader} from "@/lib/imageLoader";
-import  showcaseImage from "@/public/images/showcase-image.png";
+import { FC } from "react";
+import { Button } from "@/components/ui/button";
+import { imageLoader } from "@/lib/imageLoader";
+import showcaseImage from "@/public/images/showcase-image.png";
 
 export const Showcase: FC = () => {
-    const backgroundImageUrl = imageLoader({
-        src: `${showcaseImage.src}`,
-        width: 1250,
-        quality: 100,
-    });
+  const backgroundImageUrl = imageLoader({
+    src: `${showcaseImage.src}`,
+    width: 1250,
+    quality: 100,
+  });
 
-    return (
-        <div
-            className="h-[755px] bg-no-repeat bg-cover flex flex-col justify-end pb-[90px]"
-            style={{
-                backgroundImage: `
-          linear-gradient(to bottom, 
-                     rgba(0,0,0,0.7),  
-                     rgba(0,0,0,0.3),  
-                     rgba(0,0,0,0.3),  
-                     rgba(0,0,0,0.7)     
-),
+  return (
+    <div
+      className="sm:h-96 md:h-[755px] bg-no-repeat bg-cover flex flex-col justify-end bg-black/20 py-8 px-4 md:pb-[90px]"
+      style={{
+        backgroundImage: `
           url('${backgroundImageUrl}')
-        `
-            }}
-        >
-            <div className="container">
-                <h1 className="w-[400px] mb-3 justify-center text-white text-6xl font-normal font-title">
-                    Bags & Leather Goods
-                </h1>
-                <p className="w-64 mb-12 justify-start text-white text-base font-normal tracking-wide">
-                    New Styles In Refined Colours
-                </p>
-                <Button aria-busy={true} variant={"outline"}>Shop now</Button>
-            </div>
-        </div>
-    );
+        `,
+      }}
+    >
+      <div className="container">
+        <h1 className="w-[400px] mb-3 justify-center text-white text-xl  md:text-6xl font-normal font-title">
+          Bags & Leather Goods
+        </h1>
+        <p className="w-64 mb-12 justify-start text-white text-base font-normal tracking-wide">
+          New Styles In Refined Colours
+        </p>
+        <Button aria-busy={true} variant={"outline"} className="w-full">
+          Shop now
+        </Button>
+      </div>
+    </div>
+  );
 };
--- components/shared/news-card/news-card.tsx
+++ components/shared/news-card/news-card.tsx
@@ -1,21 +1,40 @@
-import {FC} from "react"
-import {NewsCardProps} from "@/interface/news-model.ts/news.models";
+import { FC } from "react";
+import { NewsCardProps } from "@/interface/news-model.ts/news.models";
 import Image from "next/image";
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-export const NewsCard: FC<{ newsItem: NewsCardProps }> = ({newsItem}) => {
-
-    return (
-        <div
-            className={"w-[100%] h-[100%] text-center"}>
-            <Image src={newsItem.images.src} width={500} height={500} className={"w-full h-[430px] object-cover"}
-                   alt={newsItem.text}/>
-            <h4 className={"self-stretch justify-start text-gray-800 text-xl font-medium mt-2.5 leading-9"}>{newsItem?.text}</h4>
-            <p className={'self-stretch justify-start text-gray-600 text-base font-medium leading-7 mb-6'}>{newsItem?.description}</p>
-            <Link href={`new/${newsItem.text}`} passHref={true} className={`self-stretch justify-start`}>
-                <Button variant={"secondary"}>View Now</Button>
-            </Link>
-        </div>
-    )
-}
+export const NewsCard: FC<{ newsItem: NewsCardProps }> = ({ newsItem }) => {
+  return (
+    <div className={"w-[100%] h-[100%] text-center"}>
+      <Image
+        src={newsItem.images.src}
+        width={500}
+        height={500}
+        className={"w-80 md:w-full h-[430px] object-cover"}
+        alt={newsItem.text}
+      />
+      <h4
+        className={
+          "self-stretch justify-start text-gray-800 text-xl font-medium mt-2.5 leading-9"
+        }
+      >
+        {newsItem?.text}
+      </h4>
+      <p
+        className={
+          "self-stretch justify-start text-gray-600 text-base font-medium leading-7 mb-6"
+        }
+      >
+        {newsItem?.description}
+      </p>
+      <Link
+        href={`new/${newsItem.text}`}
+        passHref={true}
+        className={`self-stretch justify-start`}
+      >
+        <Button variant={"secondary"}>View Now</Button>
+      </Link>
+    </div>
+  );
+};
