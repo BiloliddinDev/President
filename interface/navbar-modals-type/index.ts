54b4cdcdf82dc0ebe20dfe3122bc79b6c76dd012
Biloliddin_dev <bilol9876@gmail.com>
2025-06-02 12:22:22 +0500

    add nav bar section footer and some change

--- interface/navbar-modals-type/index.ts
+++ interface/navbar-modals-type/index.ts
@@ -1,8 +1,9 @@
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 
 export interface NavbarModalProps {
-  children?: ReactNode;
-  title?: string | ReactNode;
-  side?: "top" | "bottom" | "right" | "left";
-  sheetTitle?: string;
+    children?: ReactNode;
+    title?: string | ReactNode;
+    side?: "top" | "bottom" | "right" | "left";
+    sheetTitle?: string;
+    textColorClass?: string;
 }
