ea7cf4be2f581bf9421d1b66dc8dbdfb4e5a3a1e
Biloliddin_dev <bilol9876@gmail.com>
2025-06-29 20:16:07 +0500

    last change from 1 versin

--- app/[lang]/create/page.tsx
+++ app/[lang]/create/page.tsx
@@ -1,13 +1,14 @@
-import {getDictionary} from "@/lib/get-dictionary";
-import {CreateContentClient} from "./CreateContentClient";
+import { getDictionary } from "@/lib/get-dictionary";
+import { CreateContentClient } from "./CreateContentClient";
 
 interface CreatePageProps {
-    params: { lang: "uz" | "ru" | "en", category: string };
+        params: Promise<{ lang: "uz" | "ru" | "en" }>;
 }
 
-export default async function CreateContentPage({params}: CreatePageProps) {
+export default async function CreateContentPage({ params }: CreatePageProps) {
+    
+   const CreatePageParam : {lang: "uz" | "ru" | "en"} = await params;
+   const dictionary = await getDictionary(CreatePageParam.lang);
 
-    const CreatePageParam: { lang: "uz" | "ru" | "en", category: string } = await params;
-    const dictionary = await getDictionary(CreatePageParam.lang);
-    return <CreateContentClient dictionary={dictionary}/>;
+    return <CreateContentClient dictionary={dictionary} />;
 }
