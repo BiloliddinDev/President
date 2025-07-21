bfdeaada8392337d9b23a6f859fbd5a7f774b9a0
Navruza <navruzaxusniddinovna@gmail.com>
2025-07-21 18:56:33 +0500

    content changes

--- app/[lang]/(home)/components/category.tsx
+++ app/[lang]/(home)/components/category.tsx
@@ -76,13 +76,15 @@ export async function Category({ dictionary, lang }:CategoryProps){
    
     return (
         <div className="container">
+          aaaaaaaaaaaa
             <SectionTitle text={dictionary.category.title} />
             <div className="grid mt-5 md:mt-14 grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+              bbbbb
                 {CategoryData.map((category) => (
                    <Link
                    key={category.id}
                    href={`/shops/${category.nameMap?.EN}${category.id}`}
-                 > <CategoryCard 
+                 > aaaaaaa<CategoryCard 
                       key={category.id} 
                       category={category} 
                       lang={lang}
--- app/[lang]/service/appointment/page.tsx
+++ app/[lang]/service/appointment/page.tsx
@@ -1,6 +1,7 @@
 import {BreadcrumbDynamic} from "@/components/shared/breadcrumb-dynamic/breadcrumb-dynamic";
 import Image from "next/image";
-import ServicesImage from "@/public/images/services4.png";
+import ServicesImage from "@/public/images/Bag.jpg";
+// import ServicesImage from "@/public/images/services4.png";
 import BoutiqueForm from "@/app/[lang]/shops/components/boutique-form/boutique-form";
 
 export default function AppointmentPage() {
@@ -14,19 +15,18 @@ export default function AppointmentPage() {
 
             <div className="container ">
                 <div className="w-[590px] flex flex-col justify-start items-start gap-7">
-                    <h2 className="self-stretch justify-start text-primary text-lg font-medium  leading-7">Book an
-                        appointment </h2>
-                    <p className=" justify-start text-primary text-sm font-medium leading-tight">Book an appointment now
-                        at a Montblanc boutique and our staff will get in touch to arrange your personal visit to your
-                        preferred boutique.</p>
+                    <h2 className="self-stretch justify-start text-primary text-lg font-medium  leading-7">
+                    Записаться на визит</h2>
+                    <p className=" justify-start text-primary text-sm font-medium leading-tight">Оформите предварительную запись в шоурум “President Business
+Gifts”, и наш специалист свяжется с вами для организации
+персонального визита в удобное для вас время.</p>
                 </div>
             </div>
 
             <div className={"bg-neutral-100 py-7 mt-12"}>
                 <div className={'container'}>
                     <div
-                        className="self-stretch justify-start text-gray-900 text-lg font-medium font-['Inter'] leading-7">Select
-                        a boutique
+                        className="self-stretch justify-start text-gray-900 text-lg font-medium font-['Inter'] leading-7">Выберите шоурум
                     </div>
                     <BoutiqueForm/>
                 </div>
