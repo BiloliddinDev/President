e91620da1899d9b160f9f78ee17fbc37150ae65e
Biloliddin_dev <bilol9876@gmail.com>
2025-07-23 15:05:08 +0500

    fix it navbar

--- components/shared/modalContents/shopModal/index.tsx
+++ components/shared/modalContents/shopModal/index.tsx
@@ -4,16 +4,16 @@ import {shopModalItems} from "@/constants/shop-modal-item";
 // import { getCategoryModal } from "@/service/home-service/category-mobile.service";
 import Link from "next/link";
 import React from "react";
-import { Category } from "../../navbar/navbar";
+import {Category} from "../../navbar/navbar";
 
 interface shopModalProps {
     lang: "uz" | "ru" | 'en',
-    category:Category[]
+    category: Category[]
 }
 
-const ShopModalContent = ({lang,category}: shopModalProps) => {
- 
-    console.log("SHOPMODALCATEGORY",category)
+const ShopModalContent = ({lang, category}: shopModalProps) => {
+
+    console.log("SHOPMODALCATEGORY", category)
 
     return (
         <div>
@@ -24,13 +24,13 @@ const ShopModalContent = ({lang,category}: shopModalProps) => {
                      ${item.id === 9 ? "mt-10" : "my-5"}`}
                     key={item.id}
                 >
-                    {/* <Link href={`/shops/${item.nameMap.EN}${item.id}`}> */}
+                    <Link href={`/shops/${item.id}`}>
                         {item.name}
-                    {/* </Link> */}
+                    </Link>
                 </div>
             ))}
             {/* {shopModalItems.map((item) => ( */}
-            {shopModalItems.filter((i)=>i.custom===true).map((item) => (
+            {shopModalItems.filter((i) => i.custom === true).map((item) => (
                 <div
                     className={`text-sm
                      font-normal leading-[1.5rem] cursor-pointer
@@ -39,7 +39,7 @@ const ShopModalContent = ({lang,category}: shopModalProps) => {
                 >
                     <Link
                         // href={`/shops/${item.id}`}
-                       href={`/${item.link}/${item.custom ? "" : item.name[lang].toLowerCase()}`}
+                        href={`/${item.link}/${item.custom ? "" : item.name[lang].toLowerCase()}`}
                     >
                         {item.name[lang]}
                     </Link>
