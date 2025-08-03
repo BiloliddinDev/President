ec56308139c8990eeb44d021f2eddeef83789ff3
Biloliddin_dev <bilol9876@gmail.com>
2025-08-04 02:28:45 +0500

    add links

--- app/[lang]/account/order/page.tsx
+++ app/[lang]/account/order/page.tsx
@@ -1,26 +1,35 @@
 "use client"
 
 import {AccountTitle} from "@/app/[lang]/account/account-title/account-title";
-import {OrderCard} from "@/app/[lang]/account/components/order-card/order-card";
+import OrderCard from "@/app/[lang]/account/components/order-card/order-card";
 import {useEffect, useState} from "react";
 import {UserOrderService} from "@/service/order.service/user-order.service";
 import {useSession} from "next-auth/react";
+import {OrderDataInterface} from "@/interface/order-data/order-data";
+
+interface OrderInterface {
+    content: OrderDataInterface[],
+    first: boolean,
+    last: boolean,
+    page: number,
+    size: number,
+    totalElements: number,
+    totalPages: number,
+}
 
 export default function OrderPage() {
-    const [orderData, setOrderData] = useState()
+    const [orderData, setOrderData] = useState<OrderInterface>()
     const {data: session,} = useSession();
 
     useEffect(() => {
 
         const fetchLang = async () => {
-            const data = await UserOrderService(session?.user.serverData?.id);
+            const data = await UserOrderService("05bca496-ea1c-432c-b396-c432bf550dbf");
             setOrderData(data);
         };
         fetchLang().then().catch().finally();
 
     }, [session?.user.serverData?.id])
-
-    console.log(orderData, "<=== this is order data")
 
     return (
         <div>
@@ -28,9 +37,10 @@ export default function OrderPage() {
 
             <p className="text-primary text-sm font-medium leading-tight mt-3">Вы ранее не делали заказ как
                 зарегистрированный пользователь.</p>
-
             <div className={'mt-10'}>
-                <OrderCard/>
+                {orderData?.content?.map((item: OrderDataInterface) => (
+                    <OrderCard key={item.id} orderData={item}/>
+                ))}
             </div>
         </div>
     )
